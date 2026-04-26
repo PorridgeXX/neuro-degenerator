603742db008e1d15ac2e8d7b2eac28563ef0252d
por4 <steam.os@ya.ru>
2026-04-26 14:59:38 +0300

    wip: added global error handler

--- src/app/bot.ts
+++ src/app/bot.ts
@@ -1,5 +1,17 @@
 import {Bot} from "grammy";
 
 export const bot = new Bot(process.env.TELEGRAM_BOT_KEY || "");
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
+});
 
 bot.on("message", (ctx) => ctx.reply("Здарова епт"));
