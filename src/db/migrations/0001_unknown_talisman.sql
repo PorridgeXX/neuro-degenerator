b59616157201228cbd000adff0f5b3b0aaa9a52e
por4 <steam.os@ya.ru>
2026-04-27 17:32:03 +0300

    fix(db): fixed db. Added one to many linking + indexing for chat_id field

--- src/db/migrations/0001_unknown_talisman.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "mediaMessages" ALTER COLUMN "chat_id" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "messagesCounter" ALTER COLUMN "chat_id" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "textMessages" ALTER COLUMN "chat_id" SET DATA TYPE bigint;
--- src/db/schema.db.ts
+++ src/db/schema.db.ts
@@ -1,26 +1,45 @@
 import {
   pgTable,
-  PgTable,
   serial,
   text,
   integer,
   bigint,
+  index,
+  timestamp,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
-export const textMessages = pgTable("textMessages", {
-  id: serial("id").primaryKey(),
-  chatId: bigint("chat_id", { mode: "number" }).notNull(),
-  text: text("text").notNull(),
+export const mediaTypeEnum = pgEnum("media_type", ["photo", "gif"]);
+
+export const messagesCounter = pgTable("messages_counter", {
+  chatId: bigint("chat_id", { mode: "number" }).primaryKey(),
+  count: integer("count").default(0).notNull(),
 });
 
-export const mediaMessages = pgTable("mediaMessages", {
-  id: serial("id").primaryKey(),
-  chatId: bigint("chat_id", { mode: "number" }).notNull(),
-  fileId: text("file_id").notNull(),
-  fileUniqueId: text("file_unique_id").notNull(),
-});
+export const textMessages = pgTable(
+  "text_messages",
+  {
+    id: serial("id").primaryKey(),
+    chatId: bigint("chat_id", { mode: "number" })
+      .notNull()
+      .references(() => messagesCounter.chatId),
+    text: text("text").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (table) => [index("text_messages_chat_id_idx").on(table.chatId)],
+);
 
-export const messagesCounter = pgTable("messagesCounter", {
-  chatId: bigint("chat_id", { mode: "number" }).notNull(),
-  count: integer("count").default(0),
-});
+export const mediaMessages = pgTable(
+  "media_messages",
+  {
+    id: serial("id").primaryKey(),
+    chatId: bigint("chat_id", { mode: "number" })
+      .notNull()
+      .references(() => messagesCounter.chatId),
+    fileId: text("file_id").notNull(),
+    fileUniqueId: text("file_unique_id").notNull(),
+    mediaType: mediaTypeEnum().notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (table) => [index("media_messages_chat_id_idx").on(table.chatId)],
+);
