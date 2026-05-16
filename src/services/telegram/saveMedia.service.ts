29c4026e60293812d7d7e9cfc583e86c188a3e75
por4 <steam.os@ya.ru>
2026-05-17 00:13:19 +0300

    fix(saveMedia.service): Added transaction on saving file

--- src/services/telegram/saveMedia.service.ts
+++ src/services/telegram/saveMedia.service.ts
@@ -4,6 +4,7 @@ import { mkdir } from "fs/promises";
 import { db, mediaMessages } from "@/db";
 import type { PhotoMessageInput } from "@/parsers";
 import path from "path";
+import { eq } from "drizzle-orm";
 
 export const saveMediaService = async (input: PhotoMessageInput, api: Api) => {
   //checking for a media in database
@@ -36,10 +37,7 @@ export const saveMediaService = async (input: PhotoMessageInput, api: Api) => {
     dir,
     `${file.file_unique_id}${path.extname(file.file_path)}`,
   );
-  await mkdir(dir, { recursive: true });
-  await Bun.write(filePath, media);
-
-  await db
+  const inserted = await db
     .insert(mediaMessages)
     .values({
       chatId: BigInt(input.chatId),
@@ -47,5 +45,16 @@ export const saveMediaService = async (input: PhotoMessageInput, api: Api) => {
       fileUniqueId: file.file_unique_id,
       path: filePath,
     })
-    .onConflictDoNothing();
+    .onConflictDoNothing()
+    .returning({ id: mediaMessages.id });
+
+  if (!inserted[0]) return;
+
+  await mkdir(dir, { recursive: true });
+  try {
+    await Bun.write(filePath, media);
+  } catch (err) {
+    await db.delete(mediaMessages).where(eq(mediaMessages.id, inserted[0].id));
+    throw err;
+  }
 };
