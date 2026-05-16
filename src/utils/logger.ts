057c591a46fc79185f1e9712c1f34cb87f1f921d
por4 <steam.os@ya.ru>
2026-05-16 04:19:13 +0300

    refactor(logger): added local logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,28 @@ import pino from "pino";
 
 export const logger = pino({
   transport: {
-    target: "pino-pretty",
+    targets: [
+      {
+        target: "pino-pretty",
+        options: {
+          destination: 1,
+          colorize: true,
+          translateTime: "SYS:HH:MM:ss.l",
+          ignore: "pid,hostname",
+        },
+        level: "info",
+      },
+      {
+        target: "pino-pretty",
+        options: {
+          destination: "./logs/app.log",
+          mkdir: true,
+          colorize: false,
+          translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
+          ignore: "pid,hostname",
+        },
+        level: "info",
+      },
+    ],
   },
 });
