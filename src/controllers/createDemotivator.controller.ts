e4e8bb5ddb61e6a94bf13c273931ce3133cb3582
por4 <steam.os@ya.ru>
2026-05-17 03:54:39 +0300

    refactot(createDemotivatorController): Added timer between generations

--- src/controllers/createDemotivator.controller.ts
+++ src/controllers/createDemotivator.controller.ts
@@ -9,16 +9,42 @@ import {
 import type { CommandContext, Context } from "grammy";
 import { InputFile } from "grammy";
 
+class Timer {
+  private startedAt: number;
+  private delay: number;
+  constructor(callback: Function, delay: number) {
+    this.startedAt = Date.now();
+    this.delay = delay;
+    setTimeout(() => callback(), delay);
+  }
+
+  getRemaining() {
+    const elapsed = Date.now() - this.startedAt;
+    return Math.max(0, Math.ceil((this.delay - elapsed) / 1000));
+  }
+}
+
+const activeTimers: Map<number, Timer> = new Map();
+
 export const createDemotivatorController = async (
   ctx: CommandContext<Context>,
 ) => {
   try {
     const input: CommandInput = { chatId: ctx.chatId };
-    const output = await textGeneration(input);
 
-    const createdDemotivator = await createDemotivatorService(output, input);
-
-    await ctx.replyWithPhoto(new InputFile(createdDemotivator));
+    if (!activeTimers.has(input.chatId)) {
+      const output = await textGeneration(input);
+      const createdDemotivator = await createDemotivatorService(output, input);
+      await ctx.replyWithPhoto(new InputFile(createdDemotivator));
+      activeTimers.set(
+        input.chatId,
+        new Timer(() => activeTimers.delete(input.chatId), 8000),
+      );
+      return;
+    }
+    await ctx.reply(
+      `Ты намаслил свою штуку дрюку? Погодь погодь погодь ${activeTimers.get(input.chatId)?.getRemaining()}cек ты о масле в тачке?`,
+    );
   } catch (err) {
     if (err instanceof NotEnoughMessagesError) {
       await ctx.reply("Недостаточно сообщений в чате");
